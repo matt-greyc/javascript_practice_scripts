ff7895068905ecb94e403cf96c4e1acb06d65dfa
matt-greyc <69173142+matt-greyc@users.noreply.github.com>
2021-06-13 03:35:18 +0100

    Create 004_story_min_max.js

---
+++ 004_story_min_max.js
@@ -0,0 +1,40 @@
+// www.codewars.com
+// Codewars is an educational community for computer programming. On the platform, 
+// software developers train on programming challenges known as kata. These discrete
+// programming exercises train a range of skills in a variety of programming languages,
+// and are completed within an online integrated development environment.
+// On Codewars the community and challenge progression is gamified, with users
+// earning ranks and honor for completing kata, contributing kata, and quality solutions.
+
+
+// Exercise:
+// Ben has a very simple idea to make some profit: he buys something and sells it again.
+// Of course, this wouldn't give him any profit at all if he was simply to buy and sell it at
+// the same price. Instead, he's going to buy it for the lowest possible price and sell it at the highest.
+// Write a function that returns both the minimum and maximum number of the given list/array.
+// Examples:
+// minMax([1,2,3,4,5])   == [1,5]
+// minMax([2334454,5])   == [5, 2334454]
+// minMax([1])           == [1, 1]
+// All arrays or lists will always have at least one element, so you don't need to check the length.
+// Also, your function will always get an array or a list, you don't have to check for null, undefined or similar.
+
+
+function minMax(arr){
+  let lowestNumber = arr[0]
+  let highestNumber = arr[0]
+
+  for (let num of arr.slice(1)) {
+    if (num < lowestNumber) {
+      lowestNumber = num
+    }
+    if (num > highestNumber) {
+      highestNumber = num
+    }
+  }
+  return [lowestNumber, highestNumber];
+}
+
+
+// console.log(minMax([1,2,3,4,5]))
+// console.log(minMax([3]))
